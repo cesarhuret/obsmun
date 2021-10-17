968cc181d872d7970006a22c8aabd039274df9d1
Kesar <cesar.huret@gmail.com>
2021-10-17 20:17:35 +0100

    Update Registration.js

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -17,32 +17,6 @@ class Registration extends Component {
   render() {
         return ( 
         <div className="App">
-          { this.mobile ?
-          <>
-            <div style={{position: "relative"}}>
-              <img style={{maxWidth: '100%', height: 'auto'}} src='registration.jpg' alt='' />
-              <div className="center">
-                <h1 className='black' style={{fontSize: '7vh'}}>Registration</h1>
-              </div>
-            </div>
-            <br/>
-            <Container style={{textAlign: "center", paddingTop: '10%'}} fluid>
-              <Row className="align-items-center">
-                <Col lg={{span: 6, order: 1}}>
-                  <div className="rounded shadow-lg p-5" data-aos="fade-up" style={{borderStyle: "none"}}>
-                      <Nav.Link className='navlink' href='https://forms.gle/NcXXGsAE5Z2VVAZ1A' target='_blank' style={{fontSize: '3vh'}}>Register as an Individual Delegate</Nav.Link>
-                  </div>
-                </Col>
-                <Col lg={{span: 6, order: 2}}>
-                  <div className="rounded shadow-lg p-5" data-aos="fade-down" style={{borderStyle: "none"}}>
-                      <Nav.Link className='navlink' href='https://forms.gle/X25YqoNydeNnc5UWA' target='_blank' style={{fontSize: '3vh'}}>Register as a School</Nav.Link>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </>
-          : 
-          <>
             <div className='backgroundImageFull' style={{ backgroundImage: 'url("https://obsmun.org/registration.JPG")'}}>
               <Container style={{textAlign: "center", paddingLeft: '5%', paddingRight: '5%'}} fluid>
                 <Row style={{paddingTop: '15%', paddingLeft: '10%'}}>
@@ -51,12 +25,12 @@ class Registration extends Component {
                     </div>
                 </Row>
                 <Row className="align-items-center" style={{paddingTop: '5%'}}>
-                    <Col lg={{span: 6, order: 1}} style={{paddingBottom: '10px'}} data-aos="fade-down">
+                    <Col xs={{span: 12, order: 1}} lg={{span: 6, order: 1}} style={{paddingBottom: '10px'}} data-aos="fade-down">
                       <div className="shadow-lg p-5" style={{borderStyle: "none", backgroundColor: 'rgba(5, 5, 5, 0.9)', borderRadius: '1rem'}}>
                           <Nav.Link className="navlink" href='https://forms.gle/NcXXGsAE5Z2VVAZ1A' target='_blank' style={{fontSize: '3vh'}}>Register as an Individual Delegate</Nav.Link>
                       </div>
                     </Col>
-                    <Col lg={{span: 6, order: 2}} data-aos="fade-up">
+                    <Col xs={{span: 12, order: 2}} lg={{span: 6, order: 2}} data-aos="fade-up">
                       <div className="shadow-lg p-5" style={{borderStyle: "none", backgroundColor: 'rgba(5, 5, 5, 0.9)', borderRadius: '1rem'}}>
                           <Nav.Link className="navlink" href='https://forms.gle/X25YqoNydeNnc5UWA' target='_blank' style={{fontSize: '3vh'}}>Register as a School</Nav.Link>
                       </div>
@@ -64,8 +38,6 @@ class Registration extends Component {
                 </Row>
               </Container>
             </div>
-          </>
-        }
         </div>
         )
     }
