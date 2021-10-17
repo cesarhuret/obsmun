3adb82502b91cc137ab734e390823cfabd09bcec
Kesar <cesar.huret@gmail.com>
2021-10-17 19:46:21 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,13 +3,14 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import './Home.css'
 import { BrowserView, isDesktop, isMobile, MobileView } from 'react-device-detect';
 class Home extends Component {
-
+  
+  mobile = false;
   constructor() {
     super()
     if(isMobile) {
-      this.text = 'mobile'
+      this.mobile = true
     } else if(isDesktop) {
-      this.text = 'desktop' 
+      this.mobile = false
     }
   }
 
@@ -19,50 +20,50 @@ class Home extends Component {
           <h1>{this.text}</h1>
             <div className='backgroundImage' style={{ backgroundImage: 'url("https://i.imgur.com/dqAtAv6.png")'}}>
                 <Container style={{textAlign: "left", paddingBottom: '4%'}}>
-                  <BrowserView>
-                    <Row style={{paddingTop: '30%'}}>
-                      <div data-aos="fade-up">
-                        <h1 className='white' style={{fontSize: '5vw'}}>Oporto British School</h1>
-                        <h2 className='white' style={{fontSize: '3vw'}}>Model United Nations</h2>
-                        <h3 className='white' style={{fontSize: '2vw'}}>3rd to 5th February 2022</h3>
-                      </div>
-                    </Row>
-                    <Row style={{paddingTop: '10%', paddingBottom: '22%', opacity: '0.7'}} data-aos="fade-up">
-                      <div className='bounce'>
-                        <img src="arrow-circle-down.png" onClick={() => {document.getElementById('details').scrollIntoView({ behavior: 'smooth' })}}/>
-                      </div>
-                    </Row>
-                  </BrowserView>
-                  <MobileView>
-                    <Row style={{paddingTop: '30%', textAlign: "center"}}>
-                      <div data-aos="fade-up">
-                        <h1 className='white' style={{fontSize: '5vh', paddingTop: '7%'}}>Oporto British School</h1>
-                        <h2 className='white' style={{fontSize: '3vh', paddingTop: '5%'}}>Model United Nations</h2>
-                        <h3 className='white' style={{fontSize: '4vh', paddingTop: '5%'}}>3rd to 5th February 2022</h3>
-                      </div>
-                    </Row>
-                    <Row style={{paddingTop: '10%', paddingBottom: '22%', opacity: '0.7', paddingLeft: '45%'}} data-aos="fade-up">
-                      <div className='bounce'>
-                        <img src="arrow-circle-down.png" onClick={() => {document.getElementById('details').scrollIntoView({ behavior: 'smooth' })}}/>
-                      </div>
-                    </Row>
-                  </MobileView>
+                  { this.mobile ?
+                    <>
+                      <Row style={{paddingTop: '30%', textAlign: "center"}}>
+                        <div data-aos="fade-up">
+                          <h1 className='white' style={{fontSize: '5vh', paddingTop: '7%'}}>Oporto British School</h1>
+                          <h2 className='white' style={{fontSize: '3vh', paddingTop: '5%'}}>Model United Nations</h2>
+                          <h3 className='white' style={{fontSize: '4vh', paddingTop: '5%'}}>3rd to 5th February 2022</h3>
+                        </div>
+                      </Row>
+                      <Row style={{paddingTop: '10%', paddingBottom: '22%', opacity: '0.7', paddingLeft: '45%'}} data-aos="fade-up">
+                        <div className='bounce'>
+                          <img src="arrow-circle-down.png" onClick={() => {document.getElementById('details').scrollIntoView({ behavior: 'smooth' })}}/>
+                        </div>
+                      </Row>
+                    </>
+                    : <>
+                      <Row style={{paddingTop: '30%'}}>
+                        <div data-aos="fade-up">
+                          <h1 className='white' style={{fontSize: '5vw'}}>Oporto British School</h1>
+                          <h2 className='white' style={{fontSize: '3vw'}}>Model United Nations</h2>
+                          <h3 className='white' style={{fontSize: '2vw'}}>3rd to 5th February 2022</h3>
+                        </div>
+                      </Row>
+                      <Row style={{paddingTop: '10%', paddingBottom: '22%', opacity: '0.7'}} data-aos="fade-up">
+                        <div className='bounce'>
+                          <img src="arrow-circle-down.png" onClick={() => {document.getElementById('details').scrollIntoView({ behavior: 'smooth' })}}/>
+                        </div>
+                      </Row>
+                    </>
+                  }
                   <Row id="details" className="align-items-center">
                     <Col xs={{span: 12, order: 2}} lg={{span: 6, order: 1}}>
-                        <BrowserView>
-                          <div className="rounded shadow-lg p-5" data-aos="fade-right" style={{borderStyle: "none", backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
-                            <h1>Oporto British School</h1>
-                            <p>The Oporto British School is the oldest British school in continental Europe. Known for its friendly environment, the OBS takes pride in its unified community and aims to help each student fulfil their full potential in a safe and nurturing environment.</p>
-                            <Button href="/obs">More Info</Button>
-                          </div>
-                        </BrowserView>
-                        <MobileView>
-                          <div className="rounded shadow-lg p-3" data-aos="fade-up" style={{borderStyle: "none", backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
-                            <h1>Oporto British School</h1>
-                            <p>The Oporto British School is the oldest British school in continental Europe. Known for its friendly environment, the OBS takes pride in its unified community and aims to help each student fulfil their full potential in a safe and nurturing environment.</p>
-                            <Button href="/obs">More Info</Button>
-                          </div>
-                        </MobileView>
+                    { this.mobile ?
+                            <div className="rounded shadow-lg p-3" data-aos="fade-up" style={{borderStyle: "none", backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
+                              <h1>Oporto British School</h1>
+                              <p>The Oporto British School is the oldest British school in continental Europe. Known for its friendly environment, the OBS takes pride in its unified community and aims to help each student fulfil their full potential in a safe and nurturing environment.</p>
+                              <Button href="/obs">More Info</Button>
+                            </div>
+                      : <div className="rounded shadow-lg p-5" data-aos="fade-right" style={{borderStyle: "none", backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
+                          <h1>Oporto British School</h1>
+                          <p>The Oporto British School is the oldest British school in continental Europe. Known for its friendly environment, the OBS takes pride in its unified community and aims to help each student fulfil their full potential in a safe and nurturing environment.</p>
+                          <Button href="/obs">More Info</Button>
+                        </div>
+                    }
                     </Col>
                     <Col xs={{span: 12, order: 1}} lg={{span: 6, order: 2}}>
                       <div className="p-5">
@@ -77,21 +78,19 @@ class Home extends Component {
                     </Col>
                     <Col xs={{span: 12, order: 2}} lg={{span: 6, order: 2}}>
                     <div className="p-2">
-                        <BrowserView>
+                    { this.mobile ?
                           <div className="rounded shadow-lg p-5" style={{borderStyle: "none", backgroundColor: 'rgba(255, 255, 255, 0.9)'}} data-aos="fade-left">
                             <h1>2022 OBSMUN Conference</h1>
                             <p>We are glad to present the OBSMUN 2022 Conference. We hope this will provide you with the opportunity to improve your public speaking skills, whilst meeting new people and making new connections. See you there!</p>
                             <Button href="/obsmun">More Info</Button>
                           </div>
-                        </BrowserView>
-                        <MobileView>
-                          <div className="rounded shadow-lg p-3" style={{borderStyle: "none", backgroundColor: 'rgba(255, 255, 255, 0.9)'}} data-aos="fade-right">
+                      :   <div className="rounded shadow-lg p-3" style={{borderStyle: "none", backgroundColor: 'rgba(255, 255, 255, 0.9)'}} data-aos="fade-right">
                             <h1>2022 OBSMUN Conference</h1>
                             <p>We are glad to present the OBSMUN 2022 Conference. We hope this will provide you with the opportunity to improve your public speaking skills, whilst meeting new people and making new connections. See you there!</p>
                             <Button href="/obsmun">More Info</Button>
                           </div>
-                        </MobileView>
-                      </div>
+                    }
+                    </div>
                       
                     </Col>
                   </Row>
