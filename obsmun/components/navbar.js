05b69c7d209e39a66f8ebe13d06791fdd506d114
Kesar <67405604+SoloSaber@users.noreply.github.com>
2022-01-03 00:27:48 +0000

    updatre

--- obsmun/components/navbar.js
+++ obsmun/components/navbar.js
@@ -1,33 +1,28 @@
 
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import styles from './component.module.css'
 
 export default function Menu () {
     return(
-        <div>
-            <div className="fixed-top">
-                <Navbar bg='none' variant="light" expand="lg" fixed='top' style={{ position: "sticky", top: 0, backgroundColor: 'white', paddingBottom: '0'}}>
-                <Container style={{paddingBottom: '0'}}>
+            <div>
+                <Navbar className={styles.nav} bg='none' variant="light" expand="lg" fixed='top'>
+                <Container>
                     <Navbar.Brand href="/">
-                        <img src='logo192.png' style={{width: 50, height: 'auto'}} alt='' />
+                        <img src='logo512.png' style={{width: 50, height: 'auto'}} alt='' />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto" style={{fontFamily: 'Poppins', paddingBottom: '0'}}>
-                        {/* <Link className='navlink2 bottomBorder' to='/'>Home</Link>
-                        <Link className='navlink2 bottomBorder' to='/obs'>Our School</Link>
-                        <Link className='navlink2 bottomBorder' to='/obsmun'>The OBSMUN</Link>
-                        <Link className='navlink2 bottomBorder' to='/team'>The Team</Link>
-                        <Link className='navlink2 bottomBorder' to='/registration'>Registration</Link> */}
-                        <Nav.Link className="navlink2 bottomBorder" href='https://api.obsmun.org/OBSMUN%202022%20-%20Committee%20Topics.pdf'>Committee Topics</Nav.Link>
-                        <NavDropdown className="navlink2 bottomBorder" title="Contacts" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="https://www.instagram.com/obsmun2022/">Instagram</NavDropdown.Item>
-                            <NavDropdown.Item href="mailto:obsmun@obs.edu.pt">Email</NavDropdown.Item>
-                        </NavDropdown>
+                    <Nav className={styles.right}>
+                        <a className={styles.link} href='/'>Home</a>
+                        <a className={styles.link} href='/obsmun'>The Event</a>
+                        <a className={styles.link} href='/secretariat'>Secretariat</a>
+                        <a className={styles.link} href='/obs'>Our School</a>
+                        <a className={styles.link} href='/registration'>Register</a>
+                        <a className={styles.link} href='https://api.obsmun.org/OBSMUN%202022%20-%20Committee%20Topics.pdf'>Committee Topics</a>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
                 </Navbar>
             </div>
-        </div>
     )
 }
--- obsmun/pages/obsmun.js
+++ obsmun/pages/obsmun.js
@@ -1,14 +1,35 @@
 import Head from "next/head";
+import Image from "next/image";
 import styles from '../styles/Home.module.css'
 
 export default function OBSMUN () {
     return(
-        <div>
+        <div className={styles.container}>
             <Head>
-                <title>OBSMUN | The Event</title>
+                <title>OBSMUN</title>
                 <meta name="description" content="Join us at OBSMUN 2022!" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    The OBSMUN
+                </h1>
+                
+                <div className={`${styles.grid}`} data-aos='fade-up'>
+                    <div className={styles.card}>
+                        <h2>What is the OBSMUN?</h2>
+                        <p>During the OBSMUN 2022 conference, delegates will have the distinct opportunity to develop their knowledge and also, 
+                            skills alongside enthusiastic delegates from around the globe! In the majority of OBSMUN’s committees, 
+                            a clause-by-clause procedure shall have ensued; delegates may prepare draft clauses and deliberate upon them during committee sessions.
+                            Thereafter, voting on their adoption and formulating a final resolution with the inclusion of numerous clauses.
+                        </p>
+                    </div>
+                    <div className={styles.card}>
+                        {/* <Image src='/obsmuncouncil.png'/> */}
+                        <Image src="/obsmuncouncil.png" alt="Vercel Logo" width={512} height={278} />
+                    </div>    
+                </div>
+            </main>
         </div>
     )
 }
